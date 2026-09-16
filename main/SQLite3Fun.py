d7ad793905ba0f0a23955d94ae60a3318172131f
Yuzui <akirathedarktempest@gmail.com>
2026-09-16 16:42:24 +0530

    Implement CreateTable method in DataBase class
    Added CreateTable method to create a table in the database with specified columns.

--- main/SQLite3Fun.py
+++ main/SQLite3Fun.py
@@ -2,15 +2,41 @@ import sqlite3
 
 class DataBase:
     def __init__(self,dbName:str):
-        self.dbName=dbName#database name
+        self.dbName=dbName
 
-    def __close(self,commit):
-        commit.close()#this function is there to close the database, you can't use it outside, private function
-
-    def CreateDB(self):#creates the database
+    def CreateDB(self):
         if self.dbName.endswith(".db") or self.dbName.endswith(".sqlite3"):
             connect=sqlite3.connect(self.dbName)
-            self.__close(connect)
+            connect.close()
+            return self.dbName
         else:
             connect=sqlite3.connect(f"{self.dbName}.db")
-            self.__close(connect)
+            connect.close()
+            return f"{self.dbName}.db"
+
+    def CreateTable(self,table:str,columns):
+        if type(columns) is list or type(columns) is tuple:
+            create=""
+            for i in columns:
+                if type(i) is list or type(columns) is tuple:
+                    if len(i)==2:
+                        if i==columns[-1]:
+                            create+=f"{i[0]} {i[1]})"
+                        else:
+                            create+=f"{i[0]} {i[1]},"
+                    else:
+                        raise sqlite3.OperationalError(f"Can only have two arguments. For example\nids INT\nNot {i[0], i[1]}")
+                else:
+                    raise sqlite3.OperationalError("Must be given in list or tuple. For example\n[\"column_name\" \"INT\"]")
+            database=self.CreateDB()
+            connect=sqlite3.connect(database)
+            cursor=connect.cursor()
+            cursor.execute(f"CREATE TABLE IF NOT EXISTS {table}({create}")
+            connect.close()#makes a simple table, but will need many changes and error handling here
+
+"""
+from SQLite3Fun import DataBase
+database=DataBase("project.db")
+database.CreateTable("table1",[["name","TEXT"],["ids","INT"]])
+#this will make a database named project.db, and then a table with two columns name (having text, str type) and ids (having number, integer)
+"""
